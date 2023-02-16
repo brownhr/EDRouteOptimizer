b57e63d5cb54e659937fcd6b5f51735cd6fa060f
Harrison Brown <brownhr314@gmail.com>
2023-02-16 03:02:50 -0500

    Update EDSubsector.cs

--- EDRouteOptimizer/EDSubsector.cs
+++ EDRouteOptimizer/EDSubsector.cs
@@ -22,8 +22,25 @@ namespace EDRouteOptimizer
 
         // TODO: Calculate Manhattan distance between sectors
 
+        public static int ManhattanDistanceBetweenSectors(EDSector sectorA, EDSector sectorB)
+        {
+
+            int[] sectorAIDs = new int[3] { sectorA.ID64X, sectorA.ID64Y, sectorA.ID64Z };
+            int[] sectorBIDs = new int[3] { sectorB.ID64X, sectorB.ID64Y, sectorB.ID64Z };
+
+            int[] axisDistances = Enumerable.Zip(sectorAIDs, sectorBIDs, (a, b) => (int)Math.Abs(a - b)).ToArray();
+
+            return axisDistances.Sum();
+        }
+
+
+
+
+
             // TODO: Calculate Manhattan distance between subsectors
 
             // TODO: Method for determining orthogonal neighbors (and orth. + diag. neighbors?)
+
+        
     }
 }
