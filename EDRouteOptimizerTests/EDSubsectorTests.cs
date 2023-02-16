b019541cda648f4610b572b5481234c8940ce056
Harrison Brown <brownhr314@gmail.com>
2023-02-16 03:02:53 -0500

    Create EDSubsectorTests.cs

---
+++ EDRouteOptimizerTests/EDSubsectorTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EDRouteOptimizer.Tests
+{
+    [TestClass()]
+    public class EDSubsectorTests
+    {
+        [TestMethod()]
+        [DynamicData(nameof(ManhattanDistanceBetweenSectorsData))]
+        public void ManhattanDistanceBetweenSectorsTest(EDSector sectorA, EDSector sectorB, int expectedManhattanDistance)
+        {
+            int actualManhattanDistance = EDSubsector.ManhattanDistanceBetweenSectors(sectorA, sectorB);
+
+            Assert.AreEqual(actualManhattanDistance, expectedManhattanDistance);
+
+        }
+
+        private static IEnumerable<object[]> ManhattanDistanceBetweenSectorsData =>
+            new List<object[]>
+            {
+                new object[] { new EDSector("Byeia Aerb"), new EDSector("Chroarsts"), 1},
+                new object[] { new EDSector("Byua Aerb"), new EDSector("Ruekaei"), 2},
+                new object[] { new EDSector("Byua Aerb"), new EDSector("Blau Ais"), 3}
+            };
+
+    }
+}
