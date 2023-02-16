4e843d422d35cd3a307ab72a623cd19cd24e27ad
Harrison Brown <brownhr314@gmail.com>
2023-02-16 02:38:40 -0500

    Add `GalacticCoordinates` class

--- EDRouteOptimizer/BoxelCoord.cs
+++ EDRouteOptimizer/BoxelCoord.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EDRouteOptimizer
+﻿namespace EDRouteOptimizer
 {
     public class BoxelCoord
     {
@@ -44,6 +38,27 @@ namespace EDRouteOptimizer
         {
             return new int[] { x, y, z };
         }
+
+    }
+
+    public class GalacticCoordinates
+    {
+        public readonly double x;
+        public readonly double y;
+        public readonly double z;
+
+        public GalacticCoordinates(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"({x:F3}, {y:F3}, {z:F3})";
+        }
+
 
     }
 }
