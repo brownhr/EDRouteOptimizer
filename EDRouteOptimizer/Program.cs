d59218cbe989b0aabf25d097afc86957f614afff
Harrison Brown <brownhr314@gmail.com>
2023-02-17 01:14:22 -0500

    Update Program.cs

--- EDRouteOptimizer/Program.cs
+++ EDRouteOptimizer/Program.cs
@@ -5,6 +5,14 @@ namespace EDRouteOptimizer
     {
         public static void Main()
         {
+            EDSector secA = EDSector.GetSector("Eishoqs");
+            EDSector secB = EDSector.GetSector("Iowhophs");
+
+            int mDist = EDSubsector.ManhattanDistanceBetweenSectors(secA, secB);
+
+            Console.WriteLine(mDist);
+            Console.WriteLine(string.Join(", ", secA.IDArray()));
+            Console.WriteLine(string.Join(", ", secB.IDArray()));
         }
     }
 }
