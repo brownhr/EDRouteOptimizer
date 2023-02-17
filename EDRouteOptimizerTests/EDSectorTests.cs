c3706b84780490af10ddebea304d5def12dcd8d2
Harrison Brown <brownhr314@gmail.com>
2023-02-16 22:04:51 -0500

    Update EDSectorTests.cs

--- EDRouteOptimizerTests/EDSectorTests.cs
+++ EDRouteOptimizerTests/EDSectorTests.cs
@@ -5,25 +5,33 @@ namespace EDRouteOptimizer.Tests
     [TestClass()]
     public class EDSectorTests
     {
-        [TestMethod()]
-        public void ParseSectorCSVTest()
-        {
-            EDSector.ParseSectorCSV();
-        }
+        //[TestMethod()]
+        //public void ParseSectorCSVTest()
+        //{
+        //    EDSector.ParseSectorCSV();
+        //}
 
         [DataTestMethod]
         [DataRow("Graea Hypue")]
         public void InitializeSectorTest(string inputSectorName)
         {
-            EDSector sector = new EDSector(inputSectorName);
+            EDSector sector = EDSector.GetSector(inputSectorName);
             Assert.AreEqual(inputSectorName, sector.SectorName);
+        }
+
+        [TestMethod]
+        public void ReadSectorJsonTest()
+        {
+            Dictionary<string, EDSector> sectorList = EDSector.ReadJson();
+            Console.WriteLine(sectorList.Count);
+
         }
 
         [TestMethod]
         public void EDSectorKeyNotFoundException()
         {
             string notFound = "BBBBBBB";
-            Assert.ThrowsException<KeyNotFoundException>(() => new EDSector(notFound));
+            Assert.ThrowsException<KeyNotFoundException>(() => EDSector.GetSector(notFound));
         }
 
         [TestMethod()]
@@ -39,11 +47,11 @@ namespace EDRouteOptimizer.Tests
             new List<object[]>
             {
                 new object[] {new GalacticCoordinates(-1260.94,1257.75,44783.66),
-                              new EDSector("Aaefong")},
+                              EDSector.GetSector("Aaefong")},
                 new object[] {new GalacticCoordinates(844.25, 1444.03, 13312.22),
-                              new EDSector("Byeia Aerb")},
-                new object[] {new GalacticCoordinates(24870.18, 449.93, 15071.09), 
-                              new EDSector("Blaea Aip")}
+                              EDSector.GetSector("Byeia Aerb")},
+                new object[] {new GalacticCoordinates(24870.18, 449.93, 15071.09),
+                              EDSector.GetSector("Blaea Aip")}
 
             };
 
