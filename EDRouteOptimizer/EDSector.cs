82f3e87919d90c4f9e29676bfcc00f35b22d8095
Harrison Brown <brownhr314@gmail.com>
2023-02-16 22:04:48 -0500

    Update EDSector.cs

--- EDRouteOptimizer/EDSector.cs
+++ EDRouteOptimizer/EDSector.cs
@@ -1,70 +1,83 @@
 ﻿using CsvHelper.Configuration;
 using CsvHelper;
 using System.Globalization;
+using Newtonsoft.Json;
 
 namespace EDRouteOptimizer
 {
-    public class EDSector : ParseableSector
+
+
+    [JsonObject(MemberSerialization.OptIn)]
+    public class EDSector
     {
-        private static readonly string SectorCSVFilePath = @"sector_dataframe.csv";
-        private static readonly Dictionary<string, ParseableSector> SectorDict = ParseSectorCSV();
+        private static string JsonFilePath = "sector_json.json";
+        private static readonly Dictionary<string, EDSector> EDSectorDictionary = ReadJson();
+
+        [JsonProperty]
+        public string SectorName { get; set; }
+        [JsonProperty]
+        public int ID64X { get; set; }
+        [JsonProperty]
+        public int ID64Y { get; set; }
+        [JsonProperty]
+        public int ID64Z { get; set; }
+        [JsonProperty]
+        public double MaxXCoord { get; set; }
+        [JsonProperty]
+        public double MaxYCoord { get; set; }
+        [JsonProperty]
+        public double MaxZCoord { get; set; }
 
 
-        public EDSector(string sectorName)
+        public static Dictionary<string, EDSector> ReadJson()
         {
-
-            if (SectorDict.TryGetValue(sectorName, out ParseableSector? s))
+            using (StreamReader reader = new StreamReader(JsonFilePath))
             {
-                SectorName = s.SectorName;
-                MaxXCoord = s.MaxXCoord;
-                MaxYCoord = s.MaxYCoord;
-                MaxZCoord = s.MaxZCoord;
+                string json = reader.ReadToEnd();
+                List<EDSector> items = JsonConvert.DeserializeObject<List<EDSector>>(json);
 
-                ID64X = s.ID64X;
-                ID64Y = s.ID64Y;
-                ID64Z = s.ID64Z;
+                if (items == null)
+                {
+                    throw new NullReferenceException(nameof(items));
+                }
+
+                return items.ToDictionary(x => x.SectorName, x => x);
+
             }
+        }
+
+        public static EDSector GetSector(string sectorName)
+        {
+            if (EDSectorDictionary.TryGetValue(sectorName, out EDSector? sector)) { return sector; }
+
             else
             {
-                throw new KeyNotFoundException(nameof(sectorName));
+                throw new KeyNotFoundException(message: $"Invalid sector: {nameof(sectorName)}");
             }
+
+
         }
-
-        public static Dictionary<string, ParseableSector> ParseSectorCSV()
-        {
-
-            CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = true,
-                Delimiter = ","
-            };
-
-            using (StreamReader reader = new StreamReader(SectorCSVFilePath))
-            using (CsvReader csv = new CsvReader(reader, config))
-            {
-                csv.Context.RegisterClassMap<ParseableSectorMap>();
-                var records = csv.GetRecords<ParseableSector>();
-                return records.ToList().ToDictionary(x => x.SectorName, x => x);
-            }
-        }
-
-
 
         public static EDSector GetSectorFromCoords(GalacticCoordinates coords)
         {
-            foreach (ParseableSector sector in SectorDict.Values)
+            foreach (EDSector sector in EDSectorDictionary.Values)
             {
                 if (sector.CoordInSector(coords))
                 {
-                    return new EDSector(sector.SectorName);
+                    return sector;
                 }
             }
-            throw new ArgumentOutOfRangeException(nameof(coords), message: $"Coordinates {coords.ToString()} not found within any sector");
+            throw new ArgumentOutOfRangeException(nameof(coords), message: $"Coordinates {coords} not found within any sector");
         }
 
+        public bool CoordInSector(GalacticCoordinates coords)
+        {
+            bool _x = coords.x <= MaxXCoord && coords.x >= MaxXCoord - 1280;
+            bool _y = coords.y <= MaxYCoord && coords.y >= MaxYCoord - 1280;
+            bool _z = coords.z <= MaxZCoord && coords.z >= MaxZCoord - 1280;
 
-
-
+            return _x && _y && _z;
+        }
 
         public override string ToString()
         {
@@ -76,21 +89,6 @@ namespace EDRouteOptimizer
             return HashCode.Combine(SectorName);
         }
 
-        //public static bool operator ==(EDSector? s1, EDSector? s2)
-        //{
-
-        //    return s1.Equals(s2);
-        //}
-        //public static bool operator !=(EDSector? s1, EDSector? s2)
-        //{
-
-        //    return !s1.Equals(s2);
-        //}
-
-
-
-
-
         public override bool Equals(object obj)
         {
             EDSector? item = obj as EDSector;
@@ -98,48 +96,4 @@ namespace EDRouteOptimizer
             return item != null && SectorName == item.SectorName;
         }
     }
-
-    public class ParseableSector
-    {
-
-        public string SectorName { get; set; }
-        public int ID64X { get; set; }
-        public int ID64Y { get; set; }
-        public int ID64Z { get; set; }
-        public double MaxYCoord { get; set; }
-        public double MaxZCoord { get; set; }
-        public double MaxXCoord { get; set; }
-
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(SectorName);
-        }
-
-        public bool CoordInSector(GalacticCoordinates coords)
-        {
-
-            bool _x = coords.x <= MaxXCoord && coords.x >= MaxXCoord - 1280;
-            bool _y = coords.y <= MaxYCoord && coords.y >= MaxYCoord - 1280;
-            bool _z = coords.z <= MaxZCoord && coords.z >= MaxZCoord - 1280;
-
-            return _x && _y && _z;
-        }
-    }
-
-    internal class ParseableSectorMap : ClassMap<ParseableSector>
-    {
-        public ParseableSectorMap()
-        {
-            Map(p => p.SectorName).Name("SectorName");
-            Map(p => p.ID64X).Name("ID64X");
-            Map(p => p.ID64Y).Name("ID64Y");
-            Map(p => p.ID64Z).Name("ID64Z");
-
-            Map(p => p.MaxXCoord).Name("MaxXCoord");
-            Map(p => p.MaxYCoord).Name("MaxYCoord");
-            Map(p => p.MaxZCoord).Name("MaxZCoord");
-        }
-    }
-
 }
