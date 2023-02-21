b7b9e9209694af3fde775f4e745b49785ade8ab1
Harrison Brown <brownhr314@gmail.com>
2023-02-21 16:01:53 -0500

    Create AntColony.cs

---
+++ EDRouteOptimizer/AntColony.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDRouteOptimizer
+{
+
+    public partial class DistanceMatrix
+    {
+
+
+
+    }
+
+
+    internal class AntColony
+    {
+        //double[][] distanceMatrix { get; set; }
+
+
+
+        public void InitializeAnts()
+        {
+
+
+
+        }
+
+        public void InitializePheromoneMatrix()
+        {
+
+
+        }
+
+    }
+}
