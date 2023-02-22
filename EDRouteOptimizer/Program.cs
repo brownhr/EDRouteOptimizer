99a24d58687b5fd2af8d7c2298c836d472e76c63
Harrison Brown <brownhr314@gmail.com>
2023-02-21 20:55:47 -0500

    Update Program.cs

--- EDRouteOptimizer/Program.cs
+++ EDRouteOptimizer/Program.cs
@@ -7,25 +7,13 @@ namespace EDRouteOptimizer
     {
         public static void Main()
         {
-            RunNearestNeighbor();
-
-        }
-
-        public static void RunNearestNeighbor()
-        {
-
             EDSystem homeSystem = new EDSystem(systemName: "Byeia Aerb HL-Y e0",
-                                               coords: new RouteJsonCoords(x: 813.5, y: 1547.09375, z: 13291.96875));
-
-
+                                                           coords: new RouteJsonCoords(x: 813.5, y: 1547.09375, z: 13291.96875));
 
             string inputFilePath = @"C:\Users\brownhr\Desktop\test_json_parse.route";
-            EDRoute waypoints = EDRoute.ParseJson(inputFilePath);
-
+            EDRoute route = EDRoute.ParseJson(inputFilePath);
 
             string inputFSSdata = @"C:\Users\brownhr\Documents\fss.log";
-
-
             List<FSSEvent> events = FSSEvent.ParseFSSJson(inputFSSdata);
 
             List<string> mappedSystems = new List<string>();
@@ -33,7 +21,6 @@ namespace EDRouteOptimizer
             foreach (FSSEvent e in events)
             {
                 Match m = BARegex.Match(e.SystemName);
-
                 if (!m.Success) continue;
                 mappedSystems.Add(e.SystemName);
             }
@@ -42,31 +29,46 @@ namespace EDRouteOptimizer
             List<EDSystem> remaining = new List<EDSystem>();
             int sum = 0;
 
-            foreach (EDSystem s in waypoints.RouteWaypoints)
+            foreach (EDSystem s in route.RouteWaypoints)
             {
                 if (!Array.Exists(ms_array, e => e == s.SystemName))
                 {
-
                     remaining.Add(s);
                     sum++;
-
                 }
             }
 
             Console.WriteLine(sum + " Systems remaining");
-            waypoints.RouteWaypoints = remaining;
+            route.RouteWaypoints = remaining;
 
-            waypoints.RouteWaypoints.Insert(0, homeSystem);
+            route.RouteWaypoints.Insert(0, homeSystem);
 
-            DistanceMatrix distMat = new DistanceMatrix(waypoints);
+            double[,] distanceMatrix = route.GenerateDistanceMatrix();
+
+            AntColony antcolony = new AntColony(distanceMatrix);
+
+            int[] antColonySequence = antcolony.GenerateTrailSolution();
+            double finalDistance = antcolony.bestTrailLength;
+
+            Thread.Sleep(100);
+
+
+        }
+
+        public static void RunNearestNeighbor(EDRoute route)
+        {
+
+            
+
+            DistanceMatrix distMat = new DistanceMatrix(route);
             distMat.NearestNeighbor();
 
-            waypoints.SortByArray(distMat.RouteIndex);
+            route.SortByArray(distMat.RouteIndex);
 
             string timeStamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
             string outfile = @$"C:\Users\brownhr\Desktop\test_json_parse{timeStamp}.route";
 
-            waypoints.WriteJson(outfile);
+            route.WriteJson(outfile);
 
         }
     }
