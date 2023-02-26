74deb0732c802cd6cfac4a934495f588bb386143
Harrison Brown <brownhr314@gmail.com>
2023-02-26 00:12:31 -0500

    Update Form1.cs

--- EDROForms/Form1.cs
+++ EDROForms/Form1.cs
@@ -15,5 +15,35 @@ namespace EDROForms
         private void buttonUpdateOutput_Click(object sender, EventArgs e)
         {
         }
+
+        private void labelOutputSubsector_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void buttonLoadRoute_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                labelCurrentRoute.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void OptimizeRoute(string inputFilename, string outputFilename)
+        {
+
+
+
+        }
     }
 }
