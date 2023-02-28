341cb3524b61e2a19472093b794e46c8d7fc9225
Harrison Brown <brownhr314@gmail.com>
2023-02-28 01:38:57 -0500

    Create SubSectorModel.cs

---
+++ wpfEDRO/Models/SubSectorModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfEDRO.Models
+{
+    public class SubSectorModel
+    {
+        public string SubSector { get; set; }
+        public string BoxelCode { get; set; }
+
+    }
+}
